4f47d719880173744d0af11e910eede704194c43
DulatTurganbek <77954090+DulatTurganbek@users.noreply.github.com>
2021-10-24 22:35:49 +0600

    New depencies and annotations

--- books/src/main/java/com/example/books/BookApplication.java
+++ books/src/main/java/com/example/books/BookApplication.java
@@ -1,14 +1,21 @@
 package com.example.books;
 
+import io.swagger.annotations.SwaggerDefinition;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class BookApplication {
 
     public static void main(String[] args) {
@@ -21,3 +28,5 @@ public class BookApplication {
         return new RestTemplate();
     }
 }
+
+
--- books/src/main/java/com/example/books/model/Book.java
+++ books/src/main/java/com/example/books/model/Book.java
@@ -2,8 +2,12 @@ package com.example.books.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 @Builder
 public class Book {
     private Long id;
